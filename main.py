769fac2bf2872b0c1055a7639cf83e8b48de1626
Habib Sheikh <habibhamza6363@gmail.com>
2024-11-08 11:42:34 +0500

    housing_price_predictor.py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import numpy as np
 import pandas as pd
 from sklearn.datasets import make_classification
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metric import mean_squared_error
+fron sklearn.model_selection import train_test_split
+
+Dataset = pd.read_csv("your_housing_data.csv")
+X_data = Dataset.drop("price", axis = 1)
+y_data = Dataset(["price"])
+
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size = 0.2785, random_state = 42)
+
+model = Pipeline({
+    "regressor": LinearRegression(),
+    "features" : StandardScaler()
+})
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("mean_error :" ,mean_squared_error(y_pred,y_test)
+print("difference : ", y_pred, y_test)
+
+
+def predict_personal_data() : -> np.array 
