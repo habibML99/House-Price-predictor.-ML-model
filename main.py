5ee9f0dd54f2ec29ce9dbb470a43af2e88a033ba
Habib Sheikh <habibhamza6363@gmail.com>
2024-11-11 02:58:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,14 @@ print("difference : ", y_pred, y_test)
 
 
 def predict_personal_data() : -> np.array 
+ pass
+
+
+'''
+umeed he jis khushi ki is Dunia ko,
+us ko dainey Wala tou har jagha he , magar leney Wala he kon... 
+
+na Mili insaaf o idalat tou kia hoa, 
+beghair mushkil us ko yaad karta he kon...
+
+'''
